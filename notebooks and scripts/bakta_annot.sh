4becd2173a2a36e38fc7cc09bd86808311ddcc99
lvsea00 <lvsea00@gmail.com>
2026-05-08 17:24:11 +0300

    edit bakta_annot.sh

--- notebooks and scripts/bakta_annot.sh
+++ notebooks and scripts/bakta_annot.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-bakta_db download --output /beegfs/lvsea/bakta_db/ --type light
+PROJECT_DIR="/home/herbivores"
+MAGS_DIR="$PROJECT_DIR/mags_download"
+BAKTA_DB_DIR="$PROJECT_DIR/bakta_db"
+ANNOT_DIR="$PROJECT_DIR/bakta_annotations"
 
-for genome in /beegfs/lvsea/mags_download/*.fna.gz; do
+mkdir -p "$ANNOT_DIR"
+
+bakta_db download --output "$BAKTA_DB_DIR" --type light
+
+for genome in "$MAGS_DIR"/*.fna.gz; do
     sample_name=$(basename "$genome" .fna.gz)
-output_dir="${sample_name}_annot"
+    output_dir="$ANNOT_DIR/${sample_name}_annot"
 
 if [ -f "$output_dir/${sample_name}.gff3" ]; then
         echo "Skipping $sample_name - already processed" >&2
@@ -13,7 +20,7 @@ fi
 
 
 bakta \
-    --db /beegfs/lvsea/bakta_db/db-light \
+    --db "$BAKTA_DB_DIR/db-light" \
     --output "$output_dir" \
     --prefix "${sample_name}" \
     --threads 8 \
@@ -21,8 +28,7 @@ bakta \
     --min-contig-length 200 \
     --keep-contig-headers \
     --verbose \
-    "$genome" 
+    "$genome"
 
     sleep 2
 done
-
